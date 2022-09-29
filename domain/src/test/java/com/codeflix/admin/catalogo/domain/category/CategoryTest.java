4fc01625b98e55e8c97851be81c6bef07fdfd4b3
Elias Nepomuceno <eliasnepo12@gmail.com>
2022-09-28 21:51:01 -0300

    TDD for invalid name when creating category

--- domain/src/test/java/com/codeflix/admin/catalogo/domain/category/CategoryTest.java
+++ domain/src/test/java/com/codeflix/admin/catalogo/domain/category/CategoryTest.java
@@ -19,4 +19,22 @@ public class CategoryTest {
         Assertions.assertEquals(expectedDescription, category.getDescription());
         Assertions.assertEquals(expectedIsActive, category.isActive());
     }
+
+    @Test
+    public void givenAnInvalidNullName_whenCallNewCategoryAndValidate_thenShouldReceiveError() {
+        final String expectedName = null;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be null";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var actualCategory =
+                Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        final var actualException =
+                Assertions.assertThrows(DomainException.class, () -> actualCategory.validate(new ThrowsValidationHandler()));
+
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
 }
